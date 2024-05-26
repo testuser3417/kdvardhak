956aae552860f12fb2c89f1d53f8bc2c13a81412
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-05-27 01:46:06 +0530

    increase cooldown time to 10secs

--- executable_ddos.py
+++ executable_ddos.py
@@ -221,7 +221,7 @@ def start_attack_reply(message, target, port, time):
 # Dictionary to store the last time each user ran the /bgmi command
 bgmi_cooldown = {}
 
-COOLDOWN_TIME = 0
+COOLDOWN_TIME = 10
 
 
 # Handler for /bgmi command
